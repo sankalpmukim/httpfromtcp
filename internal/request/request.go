75a63954a22327781096418a913f3e1004b9b1c5
Sankalp Mukim <SANKALP.MUKIM@ibm.com>
2026-01-17 14:32:40 +0530

    seems to work with streaming but don't understand fully :'(

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,13 +2,20 @@ package request
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"strings"
 )
 
+type RequestState int
+
+const (
+	RequestStateInitialized RequestState = iota
+	RequestStateDone
+)
+
 type Request struct {
 	RequestLine RequestLine
+	state       RequestState // 0 -> initialized, 1 -> done
 }
 
 type RequestLine struct {
@@ -17,27 +24,117 @@ type RequestLine struct {
 	Method        string
 }
 
-func parseRequestLine(reqContent string) (RequestLine, error) {
-	requestLineStr := strings.Split(reqContent, "\r\n")[0]
-	reqLineParts := strings.Split(requestLineStr, " ")
-	if len(reqLineParts) != 3 {
-		return RequestLine{}, errors.New("invalid request line format")
+const bufferSize = 8
+
+func parseRequestLine(data string) (int, RequestLine, error) {
+	// Find end of request line
+	idx := strings.Index(data, "\r\n")
+	if idx == -1 {
+		// Need more data
+		return 0, RequestLine{}, nil
 	}
-	method := reqLineParts[0]
-	requestTarget := reqLineParts[1]
-	httpVersion := strings.Split(reqLineParts[2], "/")[1]
-	return RequestLine{httpVersion, requestTarget, method}, nil
+
+	line := data[:idx] // without \r\n
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return 0, RequestLine{}, errors.New("invalid request line format")
+	}
+
+	method := parts[0]
+	target := parts[1]
+
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 {
+		return 0, RequestLine{}, errors.New("invalid HTTP version")
+	}
+	version := httpParts[1]
+
+	// +2 to consume the "\r\n"
+	consumed := idx + 2
+
+	return consumed, RequestLine{
+		Method:        method,
+		RequestTarget: target,
+		HttpVersion:   version,
+	}, nil
 }
 
+// parseRequestLine re-implement
+
+// func RequestFromReader(reader io.Reader) (*Request, error) {
+// 	reqStringBytes, err := io.ReadAll(reader)
+// 	reqString := string(reqStringBytes)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Error in io.ReadAll %v", err)
+// 	}
+// 	parsedReqLine, err := parseRequestLine(reqString)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &Request{parsedReqLine}, nil
+// }
+
+// this function is called once per request, with a reader that
+// can send information in chunks
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	reqStringBytes, err := io.ReadAll(reader)
-	reqString := string(reqStringBytes)
-	if err != nil {
-		return nil, fmt.Errorf("Error in io.ReadAll %v", err)
+	buf := make([]byte, bufferSize)
+	readToIndex := 0
+
+	req := &Request{state: RequestStateInitialized}
+
+	for req.state != RequestStateDone {
+
+		// Grow if full
+		if readToIndex == len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+
+		n, err := reader.Read(buf[readToIndex:])
+		if err != nil {
+			if err == io.EOF {
+				req.state = RequestStateDone
+				break
+			}
+			return nil, err
+		}
+
+		readToIndex += n
+
+		consumed, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		if consumed > 0 {
+			// slide remaining bytes to front
+			copy(buf, buf[consumed:readToIndex])
+			readToIndex -= consumed
+		}
 	}
-	parsedReqLine, err := parseRequestLine(reqString)
-	if err != nil {
-		return nil, err
+
+	return req, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case RequestStateInitialized:
+		bytesRead, requestLine, err := parseRequestLine(string(data))
+		if err != nil {
+			return 0, err
+		}
+		if bytesRead == 0 {
+			return 0, nil // need more data
+		}
+		r.RequestLine = requestLine
+		r.state = RequestStateDone
+		return bytesRead, nil
+
+	case RequestStateDone:
+		return 0, errors.New("error: trying to read data in a done state")
+
+	default:
+		return 0, errors.New("error: unknown state")
 	}
-	return &Request{parsedReqLine}, nil
 }
